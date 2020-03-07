4dc711a3d8ca6b231e12259e20deaaa66b76503c
sherif-mohamed3 <newuser@Mohameds-MacBook-Pro.local>
2020-03-07 23:08:56 +0200

    DEVELOP: Add user model

---
+++ challenges/web-frontend/caronsale-task/src/app/shared/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User {
+    JWT: string;
+    IsAuthenticated: boolean;
+    UserId: string;
+    InternalUserId: string;
+    InternalUserUUId: string;
+    Type: number;
+    Privileges: string[]
+}
